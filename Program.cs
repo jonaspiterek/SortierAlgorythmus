48d3b10610b7d094ded2a90a6ce939a129a5517e
Jonas Piterek <piterek@kitconcept.com>
2023-01-15 20:47:55 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
-﻿using sorterAlgorythm;
-
+﻿
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] unsortedList = { 83, 2, 9, 31, 17, 54};
 
-        sortAlgorythm sortAlgorythm = new sortAlgorythm();
+        int temp;
 
-        sortAlgorythm
+        for (int i = 0; i < unsortedList.Length - 1; i++)
+        {
+            for (int j = 0; j < unsortedList.Length - (1 + i); j++)
+            {
+                if (unsortedList[j] > unsortedList[j + 1])
+                {
+                    temp = unsortedList[j + 1];
+                    unsortedList[j +1] = unsortedList[j];
+                    unsortedList[j] = temp;
+                }
+            }
+        }
+
+        foreach (int num in unsortedList)
+        {
+            Console.WriteLine(num);
+        }
+
+        Console.ReadKey();
     }
 }
